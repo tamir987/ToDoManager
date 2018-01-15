cdf231015e25446996661d8ea3e4da7e70825d76
tamir987 <tamir5021@gmail.com>
2018-01-15 17:30:55 +0200

    Added Javadoc.

--- Final project/src/Controllers/MainController.java
+++ Final project/src/Controllers/MainController.java
@@ -39,7 +39,7 @@ public class MainController extends HttpServlet {
 	}
 
 	/**
-	 * get a request and forward it to the login page - the application home
+	 * Get a request and forward it to the login page - the application home
 	 * page.
 	 * 
 	 * @param response
